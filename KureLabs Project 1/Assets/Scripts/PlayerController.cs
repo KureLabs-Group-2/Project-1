171bedc72dcab473ba27cb6a367b15f5f69e7938
elbruna37 <elbruna37@gmail.com>
2025-05-21 12:49:05 +0200

    Script PlayerController actualizado
    He puesto el salto del player para que solo pueda hacerlo cuando esta tocando el suelo

--- KureLabs Project 1/Assets/Scripts/PlayerController.cs
+++ KureLabs Project 1/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D playerRb;
+
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
+        //Physics.gravity *= gravityModifier;    //Por si queremos modificar la gravedad, de base esta a (0, -9.81f,0)
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            isOnGround = false;
+        }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
+    
+
 }
