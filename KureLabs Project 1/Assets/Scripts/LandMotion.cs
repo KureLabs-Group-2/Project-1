754238cbb26768168bd7ab0a4117d3e54e4940ec
elbruna37 <elbruna37@gmail.com>
2025-05-21 11:25:53 +0200

    Cambios del fondo y su movimiento
    He puesto un fondo temporal para comprobar el movimiento del background
    
    He actualizado el script del LandMotion para que reinicie el fondo cada vez que llegue al final
    He creado un script (MoveLeft) que mueve todo a la izquierda
    
    Hay un error en el ground, no se porque motivo pero se queda estatico

--- KureLabs Project 1/Assets/Scripts/LandMotion.cs
+++ KureLabs Project 1/Assets/Scripts/LandMotion.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class LandMotion : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 startPos;
+
+    private float repeatWidth;
+    
+
+
     void Start()
     {
         
+
+        startPos = transform.position;   //iguala la posición del background y ground a la posicion 0
+
+
+        repeatWidth = GetComponent<BoxCollider2D>().size.x / 2;    //guarda en la variable la mitad del ancho del background
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
+        if (transform.position.x < startPos.x - repeatWidth)     //compara la posicion x del objeto con la mitad del ancho
+        {
+            transform.position = startPos;        //si la condición se cumple regresa el objeto a la posición inicial
+        }
     }
 }
